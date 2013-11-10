b0db151e80ce3f10b95d8d05096e152231b29a55
Sarah Rachmiel <srachmiel2014@kellogg.northwestern.edu>
2013-11-10 12:16:45 -0600

    API Project 1: Phase I

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 AddressToCoords::Application.routes.draw do
-  get("/coords", { :controller => "addresses", :action => "fetch_coordinates" })
+  get("/coords/", { :controller => "addresses", :action => "fetch_coordinates" })
 end
